7ee1b15ea3b105b1b99241e760696cfcc439adc0
Jack Bailey <jackbailey261@gmail.com>
2020-04-14 21:49:56 +0100

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,13 @@
-resource "aws_elasticsearch_domain" "example" {
-    domain_name           = "example"
-    elasticsearch_version = "1.5"
+provider "aws" {
+    access_key = "AKIATIKCPEX4BVRPLTSV"
+    secret_key = "y4B5zdkWXnYKs5H8G8MQ46NTZbla2FvgQRV2ekmN"
+    region = "eu-west-2"
+}
 
-    cluster_config {
-        instance_type = "t2.medium.elasticsearch"
-    }
-
-    snapshot_options {
-        automated_snapshot_start_hour = 23
-    }
-
-    tags = {
-        Domain = "TestDomain"
+resource "aws_instance" "example" {
+    ami = "ami-01a6e31ac994bbc09"
+    instance_type = "t2.micro"
+    tags {
+        Name = "your-instance"
     }
 }
