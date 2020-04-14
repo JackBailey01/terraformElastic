31b4cb634e1c7147f45ab3039b2d0d44fa290487
Jack Bailey <jackbailey261@gmail.com>
2020-04-14 22:32:14 +0100

    no creds2

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
     region = "us-east-2"
 }
 
-resource "aws_instance" "example2" {
+resource "aws_instance" "example3" {
     ami = "ami-0f7919c33c90f5b58"
     instance_type = "t2.micro"
 
