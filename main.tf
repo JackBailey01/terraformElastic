3dc91a0ced1d6da4062e14e753b4ff06e0aac455
Jack Bailey <jackbailey261@gmail.com>
2020-04-14 22:02:57 +0100

    test

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ provider "aws" {
     region = "eu-west-2"
 }
 
-resource "ec2" "example" {
+resource "aws_instance" "example" {
     ami = "ami-01a6e31ac994bbc09"
     instance_type = "t2.micro"
     tags {
