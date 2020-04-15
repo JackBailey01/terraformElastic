92659397e0aba73602b1a8f8c0c11fc65d985307
Jack Bailey <jackbailey261@gmail.com>
2020-04-15 13:52:34 +0100

    opps

--- main.tf
+++ main.tf
@@ -1,10 +1,8 @@
 provider "aws" {
-    access_key = "access"
-    secret_key = "secret"
     region = "eu-west-2"
 }
 
-resource "aws_instance" "example3" {
+resource "aws_instance" "gotIt" {
     ami = "ami-0f7919c33c90f5b58"
     instance_type = "t2.micro"
 
---
+++ variables.tf
@@ -0,0 +1,3 @@
+variable "aws_account_id" {
+default = 224014575096
+}
